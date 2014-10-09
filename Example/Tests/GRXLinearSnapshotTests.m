6766b07f9ac73e8ee2df461724efd2c7f7742b71
Jose Alcalá Correa <jose.alcala.correa@gmail.com>
2014-10-09 23:31:38 +0200

    Fix a typo

--- Example/Tests/GRXLinearSnapshotTests.m
+++ Example/Tests/GRXLinearSnapshotTests.m
@@ -16,7 +16,7 @@
     self.recordMode = NO;
 }
 
-- (void)testVertical {§
+- (void)testVertical {
     GRXLayoutInflater *inflater = [GRXTestHelper inflaterForFileWithName:@"linear_vertical.grx"];
 
     UIImageView *imageView = [inflater viewForIdentifier:@"image"];
